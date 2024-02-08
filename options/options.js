4c0a316479f423e799f42d733b701a679057b712
Crowdbotics <team@crowdbotics.com>
2024-02-08 19:57:22 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mobile-coupon-46154",
+  url: "https://mobile-coupon-46154.botics.co",
+  api: "https://mobile-coupon-46154.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
